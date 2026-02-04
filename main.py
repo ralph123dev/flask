14af31284bb7e1a64d4ddb4b6004868cf7e9f92d
ralph123 <ralphurgue@gmail.com>
2026-02-04 21:09:35 +0100

    mise a jour du first

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Bonjour les gens! ")
+from flask import Flask 
+
+#initialisation de l'application Flask
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "<h1>salut la team</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
